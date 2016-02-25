69729b570b563a6c848e81fbf988a3afde1305bf
Jacson Passold <jacson@ethymos.com.br>
2016-02-25 15:18:36 -0300

    prod logincidadao

--- LoginCidadaoStrategy.php
+++ LoginCidadaoStrategy.php
@@ -14,7 +14,8 @@
  * 
  * @package			Opauth.LoginCidadao
  */
-class LoginCidadaoStrategy extends OpauthStrategy {
+class LoginCidadaoStrategy extends OpauthStrategy
+{
 	
 	/**
 	 * Compulsory config keys, listed as unassociative arrays
@@ -32,10 +33,16 @@ class LoginCidadaoStrategy extends OpauthStrategy {
 	 */
 	public $defaults = array(
 		'redirect_uri' => '{path_to_strategy}oauth2callback',
-		'scope' => 'email public_profile',
+		'scope' => 'email public_profile logout',
 		'response_type' => 'code'
 	);
 	
+	/**
+	 * Constructor, change defaults array for get online url
+	 * 
+	 * @param array $strategy Strategy-specific configuration
+	 * @param array $env Safe env values from Opauth, with critical parameters stripped out
+	 */
 	public function __construct($strategy, $env)
 	{
 		parent::__construct($strategy, $env);
@@ -45,15 +52,21 @@ class LoginCidadaoStrategy extends OpauthStrategy {
 	/**
 	 * Auth request
 	 */
-	public function request() {
-		$url = $this->strategy['url_base'].'/web/app_dev.php/oauth/v2/auth';
+	public function request()
+	{
+		// on Dev
+		//$url = $this->strategy['url_base'].'/web/app_dev.php/oauth/v2/auth';
+		// On Prod
+		$url = $this->strategy['url_base'].'/oauth/v2/auth';
+		
 		$params = array(
 			'client_id' => $this->strategy['client_id'],
 			'redirect_uri' => $this->strategy['redirect_uri'],
 			
 		);
 
-		foreach ($this->optionals as $key) {
+		foreach ($this->optionals as $key)
+		{
 			if (!empty($this->strategy[$key])) $params[$key] = $this->strategy[$key];
 		}
 		$this->clientGet($url, $params);
@@ -62,10 +75,15 @@ class LoginCidadaoStrategy extends OpauthStrategy {
 	/**
 	 * Internal callback, after OAuth
 	 */
-	public function oauth2callback() {
-		if (array_key_exists('code', $_GET) && !empty($_GET['code'])) {
+	public function oauth2callback()
+	{
+		if (array_key_exists('code', $_GET) && !empty($_GET['code']))
+		{
 			$code = $_GET['code'];
-			$url = $this->strategy['url_base'].'/web/app_dev.php/oauth/v2/token';
+			// on Dev use:
+			//$url = $this->strategy['url_base'].'/web/app_dev.php/oauth/v2/token';
+			
+			$url = $this->strategy['url_base'].'/oauth/v2/token';
 			
 			$params = array(
 				'code' => $code,
@@ -113,7 +131,8 @@ class LoginCidadaoStrategy extends OpauthStrategy {
 				
 				$this->callback();
 			}
-			else {
+			else
+			{
 				$error = array(
 					'code' => 'access_token_error',
 					'message' => 'Failed when attempting to obtain access token',
@@ -126,7 +145,8 @@ class LoginCidadaoStrategy extends OpauthStrategy {
 				$this->errorCallback($error);
 			}
 		}
-		else {
+		else
+		{
 			$error = array(
 				'code' => 'oauth2callback_error',
 				'raw' => $_GET
@@ -142,12 +162,19 @@ class LoginCidadaoStrategy extends OpauthStrategy {
 	 * @param string $access_token 
 	 * @return array Parsed JSON results
 	 */
-	private function user($access_token) {
-		$user = $this->serverGet( $this->strategy['url_base'].'/web/app_dev.php/api/v1/person.json', array('access_token' => $access_token), null, $headers);
-		if (!empty($user)) {
+	private function user($access_token)
+	{
+		// On dev use:
+		//$user = $this->serverGet( $this->strategy['url_base'].'/web/app_dev.php/api/v1/person.json', array('access_token' => $access_token), null, $headers);
+		
+		$user = $this->serverGet( $this->strategy['url_base'].'/api/v1/person.json', array('access_token' => $access_token), null, $headers);
+		
+		if (!empty($user))
+		{
 			return $this->recursiveGetObjectVars(json_decode($user));
 		}
-		else {
+		else
+		{
 			$error = array(
 				'code' => 'userinfo_error',
 				'message' => 'Failed when attempting to query the Do API for user information',
@@ -162,6 +189,8 @@ class LoginCidadaoStrategy extends OpauthStrategy {
 	}
 	
 	/**
+	 * Curl Hack
+	 * 
 	 * Simple server-side HTTP request with file_get_contents
 	 * Provides basic HTTP calls.
 	 * See serverGet() and serverPost() for wrapper functions of httpRequest()
@@ -179,13 +208,19 @@ class LoginCidadaoStrategy extends OpauthStrategy {
 	public static function httpRequest($url, $options = null, &$responseHeaders = null)
 	{
 		$context = null;
-		if (!empty($options) && is_array($options)) {
-			if (empty($options['http']['header'])) {
+		if (!empty($options) && is_array($options))
+		{
+			if (empty($options['http']['header']))
+			{
 				$options['http']['header'] = "User-Agent: opauth";
-			} else {
+			}
+			else
+			{
 				$options['http']['header'] .= "\r\nUser-Agent: opauth";
 			}
-		} else {
+		}
+		else
+		{
 			$options = array('http' => array('header' => 'User-Agent: opauth'));
 		}
 		$context = stream_context_create($options);
